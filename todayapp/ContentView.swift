193f26b5f3be03528d89773db58d8ff0caf63663
bradyap <bpettengill3@gmail.com>
2024-09-25 00:26:43 -0400

    xcode sucks

--- todayapp/ContentView.swift
+++ todayapp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to Today! \n - Brady")
         }
         .padding()
     }
